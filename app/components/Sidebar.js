8898d0ff2c1666620daa8136113a9777857b63f8
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2026-01-10 00:09:26 +0200

    Fix Sidebar component import statement: added missing semicolon in Sidebar.js for improved code consistency.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter, usePathname } from "next/navigation";
 import { FaTimes, FaHome, FaUsers, FaChartBar, FaCog } from "react-icons/fa";
-import styles from "./Sidebar.module.css"
+import styles from "./Sidebar.module.css";
 export default function SideBar({ isOpen, onClose, userRole }) {
   const router = useRouter();
   const pathname = usePathname();
